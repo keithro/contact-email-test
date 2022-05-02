1702876eab9e2491199610eb82a1dfee1312f88a
keithro <rodriguez.keith@gmail.com>
2022-05-01 22:07:52 -0500

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,56 +15,49 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Contact Form
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Please be a human!
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+          <style jsx>{`
+              label {
+                display: block;
+                margin-botton: .2em;
+              }
+              button {
+                color: white;
+                background-color: blueviolet;
+                padding: .8em 1em;
+                border: 0;
+                border-radius: .2em;
+              }
+            `}</style>
+          <form method='post' onSubmit={handleOnSubmit}>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              <label htmlFor='name'>Name</label>
+              <input type='text' name='name' />
             </p>
-          </a>
+            <p>
+              <label htmlFor='email'>Email</label>
+              <input type='email' name='email' />
+            </p>
+            <p>
+              <label htmlFor='message'>Message</label>
+              <textarea name='message' />
+            </p>
+            <p>
+              <button>Submit</button>
+            </p>
+          </form>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        
       </footer>
     </div>
   )
